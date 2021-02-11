9eeb5b0e913cc158bea1e306dbecf52747f5bb84
Nicoll Oliver <nicolldon96@gmail.com>
2021-02-11 17:04:30 -0500

    commit tree 84184a5f8d286991701e13421e373226951b8aac

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,67 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    # song.name = name
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name) 
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+    #or you can do below, remember pipe is conditional 
+    # if self.find_by_name(name) == nil
+    #   self.create_by_name(name)
+    # else
+    #   self.find_by_name(name)
+    # end
+
+  end
+  
+  def self.alphabetical()
+    @@all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    #similar to csv data example
+    song = self.new 
+    song.name = (filename.split(" - ") [1].chomp(".mp3"))
+    song.artist_name = (filename.split(" - ") [0])
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new
+    song.name = (filename.split(" - ") [1].chomp(".mp3"))
+    song.artist_name = (filename.split(" - ") [0])
+    @@all << song
+    song
+  end
+
+  def self.destroy_all()
+    # @@all.clear
+    self.all.clear
+  end
+
 end
+
+
+
